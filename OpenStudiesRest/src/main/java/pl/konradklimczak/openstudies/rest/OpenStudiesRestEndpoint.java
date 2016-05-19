deee3716fe2a56318d560a0d5151ff333ee2df39
Konrad Klimczak <konrad.klimczak@comarch.com>
2016-05-19 17:58:30 +0200

    small changes in project dependencies

--- OpenStudiesRest/src/main/java/pl/konradklimczak/openstudies/rest/OpenStudiesRestEndpoint.java
+++ OpenStudiesRest/src/main/java/pl/konradklimczak/openstudies/rest/OpenStudiesRestEndpoint.java
@@ -1,13 +1,11 @@
 package pl.konradklimczak.openstudies.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.konradklimczak.openstudies.data.Subject.SubjectDto;
 import pl.konradklimczak.openstudies.service.DataService;
 
-@ComponentScan("pl.konradklimczak.openstudies.service")
 @RestController
 public class OpenStudiesRestEndpoint {
 
