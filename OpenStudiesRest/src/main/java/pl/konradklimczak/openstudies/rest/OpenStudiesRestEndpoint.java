9f9ceadcbf9361134bca0235ae40d100237e49a3
Konrad Klimczak <konrad.klimczak@comarch.com>
2016-05-20 00:11:08 +0200

    add rest method which return collection

--- OpenStudiesRest/src/main/java/pl/konradklimczak/openstudies/rest/OpenStudiesRestEndpoint.java
+++ OpenStudiesRest/src/main/java/pl/konradklimczak/openstudies/rest/OpenStudiesRestEndpoint.java
@@ -2,9 +2,12 @@ package pl.konradklimczak.openstudies.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.konradklimczak.openstudies.data.Subject.SubjectDto;
 import pl.konradklimczak.openstudies.service.DataService;
+
+import java.util.List;
 
 @RestController
 public class OpenStudiesRestEndpoint {
@@ -15,5 +18,10 @@ public class OpenStudiesRestEndpoint {
     @RequestMapping("/subject")
     public SubjectDto getMySubject () {
         return dataService.getMySubjectToRest();
+    }
+
+    @RequestMapping(value = "/subjects", method = RequestMethod.GET)
+    public List<SubjectDto> getMySubjects () {
+        return dataService.getMySubjects();
     }
 }
--- OpenStudiesService/src/main/java/pl/konradklimczak/openstudies/service/DataService.java
+++ OpenStudiesService/src/main/java/pl/konradklimczak/openstudies/service/DataService.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Service;
 import pl.konradklimczak.openstudies.data.Subject.Subject;
 import pl.konradklimczak.openstudies.data.Subject.SubjectDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class DataService {
 
     public SubjectDto getMySubjectToRest() {
         return Subject.builder().name("WDI").description("Nice subject!").build().asDto();
+    }
+
+    public List<SubjectDto> getMySubjects() {
+        List<SubjectDto> subjectDtos = new ArrayList<SubjectDto>();
+        for (int i = 0; i < 10; i++) {
+            subjectDtos.add(Subject.builder().name("WDI" + i).description("Nice subject!").build().asDto());
+        }
+        return subjectDtos;
     }
 }
