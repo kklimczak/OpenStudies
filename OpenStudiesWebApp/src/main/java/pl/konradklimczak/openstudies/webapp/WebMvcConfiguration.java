7cb0446e4dc668b586e628c4d011812c56346f44
Konrad Klimczak <konrad.klimczak@comarch.com>
2016-05-22 23:26:58 +0200

    add view redirect to addViewController method

--- OpenStudiesWebApp/src/main/java/pl/konradklimczak/openstudies/webapp/WebMvcConfiguration.java
+++ OpenStudiesWebApp/src/main/java/pl/konradklimczak/openstudies/webapp/WebMvcConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     public void addViewController(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/");
         registry.addViewController("/").setViewName("forward:static/index.html");
     }
+
+
 }
